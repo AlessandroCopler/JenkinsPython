429ad606f7c2a831970ff535408e3ba6d25478df
AlessandroCopler <alessandro.copler@gmail.com>
2015-10-13 15:12:41 +0200

    Test somma

--- HelloWorld/MainPackage/test.py
+++ HelloWorld/MainPackage/test.py
@@ -6,7 +6,7 @@ class Test(unittest.TestCase):
 
 
     def testName(self):
-        self.assertEqual(sum(2,3),5,"Test riuscito")  
+        self.assertEqual(sum(2,3),5,"La somma è sbagliata")  
 
 
 if __name__ == "__main__":
